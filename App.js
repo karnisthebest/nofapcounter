5186e6acce2f27252161897409efb3f697cae1c8
matong <karnisthebest@gmail.com>
2020-07-20 23:03:17 +0700

    [Added] Auth stack and App stack

--- App.js
+++ App.js
@@ -1,25 +1,64 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './Screens/HomeScreen'
-import SignInScreen from './Screens/SigninScreen'
+import React, { useEffect } from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./Screens/HomeScreen";
+import SignInScreen from "./Screens/SigninScreen";
+import getTheme from "./native-base-theme/components";
+import material from "./native-base-theme/variables/material";
+import { StyleProvider } from "native-base";
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import SignUpScreen from "./Screens/SignUpScreen";
 
 const Stack = createStackNavigator();
 
-function App() {
+const AppStack = createStackNavigator();
+const AuthStack = createStackNavigator();
+
+const AuthScreens = () => {
   return (
-    <NavigationContainer>
-    <Stack.Navigator>
-      {false ? (
-        <>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </>
-      ) : (
-        <Stack.Screen name="SignIn" component={SignInScreen} options={{headerShown: false}}/>
-      )}
-    </Stack.Navigator>
-  </NavigationContainer>
+    <AuthStack.Navigator>
+    <AuthStack.Screen
+      name="SignIn"
+      component={SignInScreen}
+      options={{
+        headerShown: false,
+      }}
+    />
+    <AuthStack.Screen name="SignUp" component={SignUpScreen} />
+  </AuthStack.Navigator>
+  )
+}
+
+const AppScreens = () => {
+  return (
+ <AppStack.Navigator>
+    <AppStack.Screen
+      name="Home"
+      component={HomeScreen}
+      options={{
+        title: "Home",
+      }}
+    />
+  </AppStack.Navigator>
+  )
+};
+function App() {
+  let [fontsLoaded] = useFonts({
+    Roboto: require('native-base/Fonts/Roboto.ttf'),
+    Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+  const isLoggedIn = false;
+  return (
+    <StyleProvider style={getTheme(material)}>
+       <NavigationContainer>
+      {!isLoggedIn ? <AuthScreens /> : <AppScreens />}
+    </NavigationContainer>
+    </StyleProvider>
   );
 }
 
--- Screens/SigninScreen.js
+++ Screens/SigninScreen.js
@@ -1,14 +1,76 @@
-
-import * as React from 'react';
-import { ImageBackground } from 'react-native';
-import {Text} from 'native-base'
-import MountainBackground from '../assets/mountaincloud.jpg'
-function HomeScreen({navigation}) {
-    return (
-        <ImageBackground source={MountainBackground} style={{width: '100%', height: '100%'}}>
-        <Text style={{color: 'white'}}>Inside</Text>
-      </ImageBackground>
-    );
+import * as React from "react";
+import {
+  ImageBackground,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
+import { Text, View, Item, Input, Button } from "native-base";
+import { useForm } from "react-hook-form";
+import MountainBackground from "../assets/nightsky.jpg";
+function HomeScreen({ navigation }) {
+  const { register, setValue, handleSubmit, errors } = useForm();
+  const onSubmit = data => {
+    console.log(data, 'data')
   }
+  
+  React.useEffect(() => {
+    register({ name: 'username'}, { required: true });
+    register({ name: 'password'}, { required: true });
+  }, [register])
+
+  return (
+    <ImageBackground
+      source={MountainBackground}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS == "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.container}>
+          <Text style={{ color: "white", fontSize: 30 }}>FAP WATCH</Text>
+          <Item rounded style={{ width: "90%", marginTop: 20 }}>
+            <Input
+              autoCapitalize="none"
+              placeholderTextColor="snow"
+              placeholder="username"
+              style={{ color: "white" }}
+              onChangeText={text => setValue('username', text, true)}
+            />
+          </Item>
+          <Item rounded style={{ width: "90%", marginTop: 10 }}>
+            <Input
+              secureTextEntry
+              autoCapitalize="none"
+              placeholderTextColor="snow"
+              placeholder="password"
+              style={{ color: "white" }}
+              onChangeText={text => setValue('password', text, true)}
+            />
+          </Item>
+          <Button
+            warning
+            block
+            rounded
+            color="black"
+            style={{ marginHorizontal: 20, marginTop: 20 }}
+            onPress={handleSubmit(onSubmit)}
+          >
+            <Text>LOGIN</Text>
+          </Button>
+        </View>
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  );
+}
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
